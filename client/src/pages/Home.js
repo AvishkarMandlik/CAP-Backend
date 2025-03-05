c430e0bb25faf32202a62d906d4c47bc596a5bea
AvishkarMandlik <mandlikavi121@gmail.com>
2025-03-05 18:54:25 +0530

    Add SweetAlert2 dependency and enhance Home page layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Pdftest from './Pdftest'
 import Footer from '../components/Footer'
 
 function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
     <Navbar />
+  
+    <main className="flex-grow container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Home</h1>
+      
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <p className="text-gray-700">Welcome to the home page! Add your content here.</p>
+      </div>
+    </main>
+  
     <Footer />
   </div>
-  
   )
 }
 
